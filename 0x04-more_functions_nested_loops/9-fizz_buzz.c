ba7e4999646f7d0cb74dc9274dfe8eedaf2f2479
bashyitsipacifique <drichshema@gmail.com>
2023-02-23 05:17:51 -0800

    Task 2

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -1,30 +1,26 @@
 #include "main.h"
+#include <stdio.h>
 
 /**
- * print_square - draws a square
- * @size: length and width of square
- *
- * Return: void
+ * main - Entry piont
+ * Return: Always 0
  */
-
-void print_square(int size)
+int main(void)
 {
-	int row;
-	int column;
+	int i = 1;
 
-	if (size > 0)
+	for (; i < 100 ; i++)
 	{
-		for (row = 0; row < size; row++)
-		{
-			for (column = 0; column < size; column++)
-			{
-				_putchar('#');
-			}
-			_putchar('\n');
-		}
+		if (i % 3 == 0 && i % 5 == 0)
+			printf("FizzBuzz ");
+		else if (i % 3 == 0)
+			printf("Fizz ");
+		else if (i % 5 == 0)
+			printf("Buzz ");
+		else
+			printf("%d ", i);
 	}
-	else
-	{
-		_putchar('\n');
-	}
+	printf("Buzz\n");
+
+	return (0);
 }
